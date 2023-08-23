fa1a7212be800436d3d4bc6d4c4eae1339858bb4
AleksandraRolka <arolka@interia.com>
2023-08-23 22:31:29 +0000

    Update scripts,  jupyter notebooks and models

--- data-preprocessing/scripts/users/.ipynb_checkpoints/5_add_mapped_country_columns-checkpoint.py
+++ data-preprocessing/scripts/users/.ipynb_checkpoints/5_add_mapped_country_columns-checkpoint.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-# from gcp_env import PROJECT_ID, LOCATION
+from gcp_env import PROJECT_ID, LOCATION
 
 import pandas as pd
 
@@ -80,35 +80,50 @@ print("Loading data from source table..")
 SQL_QUERY = f"""SELECT * FROM {BQ_SRC_TABLE_USERS}"""
 users_df = bqclient.query(SQL_QUERY).to_dataframe()
 
-# in case script is interrupted
-# users_df = users_df[1655:]
 
 ## Add column with country name and mapped numeric value in another column
 print("Mapping location - country info..")
+
+dest_table_name = "users_with_country_temp" # Destination BigQuery table name
+BQ_DEST_TABLE_USERS = dataset + "." + dest_table_name
+path_to_dest_schema = "./../../schema/users_schema_preprocessed_reduced_with_country.json"
+# path_to_dest_schema = "./users_schema_preprocessed_reduced_with_country.json"
 
 job_config = bigquery.LoadJobConfig(
         schema=bqclient.schema_from_json(path_to_dest_schema)
 )
 job_config.write_disposition = bigquery.WriteDisposition.WRITE_APPEND
 
-dest_table_name = "users_with_country" # Destination BigQuery table name
-BQ_DEST_TABLE_USERS = dataset + "." + dest_table_name
-path_to_dest_schema = "./../../schema/users_schema_preprocessed_reduced_with_country.json"
-# path_to_dest_schema = "./users_schema_preprocessed_reduced_with_country.json"
+org_users_df = pd.DataFrame(users_df).copy()
+max_rows = len(users_df)
+start_indx = 0
+max_num_rows_in_single_df = 200
 
-for index, record in users_df.iterrows():
-    country = map_to_country(record)
-    if (country == 'United States of America'):
-        country = 'United States'
-    record['country'] = country
-    record['country_numeric'] = map_country_name_to_num_class(record['country'])
-
+while (start_indx < max_rows):
+    new_df = pd.DataFrame(columns=(users_df.columns))
+    print("Curr start_indx = ", start_indx)
+    users_df = org_users_df[start_indx:]
+    new_df = pd.DataFrame(columns=(users_df.columns))
+    k = 0
+    for index, record in users_df.iterrows():
+        if (k >= max_num_rows_in_single_df):
+            break
+        country = map_to_country(record)
+        if (country == 'United States of America'):
+            country = 'United States'
+        record['country'] = country
+        record['country_numeric'] = map_country_name_to_num_class(record['country'])
+        # new_df.append(record)
+        new_df = pd.concat([new_df, record.to_frame().T])
+        k += 1
+    start_indx += k
     # Save new dataframe to Big Query Table
     load_job = bqclient.load_table_from_dataframe(
-        pd.DataFrame([record]), BQ_DEST_TABLE_USERS, job_config=job_config
+        pd.DataFrame(new_df).copy(), BQ_DEST_TABLE_USERS, job_config=job_config
     )
     # Wait for the job to complete
     load_job.result()
+    print("Next batch saved")
 
 print("All records saved in  destination Big Query table")
 print("DONE")
